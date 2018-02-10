4d173c4c32928df43f2a6d93f1e79f2c2ec22b3e
Christian Haller <christian.haller@otto.de>
2018-02-10 21:45:43 +0100

    invoke

---
+++ invoke.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const functionName = JSON.parse(fs.readFileSync('./infrastructure/terraform.tfstate', 'utf8')
+  .toString()).modules[0].resources['aws_lambda_function.lambda'].primary.id;
+const region = JSON.parse(fs.readFileSync('./infrastructure/terraform.tfstate', 'utf8')
+  .toString()).modules[0].resources['aws_lambda_function.lambda'].primary.attributes['environment.0.variables.REGION'];
+const AWS = require('aws-sdk');
+
+console.log(region);
+
+const lambda = new AWS.Lambda({
+  region,
+});
+
+
+const params = {
+  ClientContext: Buffer.from(JSON.stringify({})).toString('base64'),
+  FunctionName: functionName,
+  InvocationType: 'RequestResponse',
+  LogType: 'Tail',
+  Payload: '{ "queryStringParameters": { "url": "http%3A%2F%2Fwww.tripadvisor.com%2Fmembers%2Fchristianhaller" } }',
+};
+lambda.invoke(params, (err, data) => {
+  if (err) {
+    console.log(err, err.stack);
+  } else {
+    console.log(data);
+  }
+});
