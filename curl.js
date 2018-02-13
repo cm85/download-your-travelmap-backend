be31c99f32c2aea187b2c58e134bd8d919138bd4
Christian Haller <christian.haller@otto.de>
2018-02-13 20:06:48 +0100

    stand der dinge

--- curl.js
+++ curl.js
@@ -9,21 +9,24 @@ const aRecordHost = `https://${JSON.parse(fs.readFileSync('./infrastructure/terr
 const { path } = JSON.parse(fs.readFileSync('./infrastructure/terraform.tfstate', 'utf8')
   .toString()).modules[0].resources['aws_api_gateway_resource.resource'].primary.attributes;
 
-console.log(gatewayHost);
-console.log(aRecordHost);
-
 const query = '?url=http%3A%2F%2Fwww.tripadvisor.com%2Fmembers%2Fchristianhaller';
+console.log(gatewayHost);
+console.log(aRecordHost + path + query);
+
 
 require('node-fetch')(gatewayHost + path + query)
-  .then((res) => {
+  .then(async (res) => {
     console.log(res.status);
     console.log(res.headers.raw());
+    // console.log(await res.text());
   });
 
 
 require('node-fetch')(aRecordHost + path + query)
-  .then((res) => {
+  .then(async (res) => {
     console.log(res.status);
     console.log(res.headers.raw());
+    // console.log(await res.text());
   });
 
+// window.fetch('https://api-dytm2018.christianhaller.com/free-my-map?url=http%3A%2F%2Fwww.tripadvisor.com%2Fmembers%2Fchristianhaller').then(async(res)=>{console.log(await res.text())});
