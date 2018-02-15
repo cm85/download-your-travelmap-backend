ab6ddfa1a6e37d4f6b22c12289742ecc796ccef7
Christian Haller <christian.haller@otto.de>
2018-02-15 11:30:27 +0100

    caching

--- app/response.js
+++ app/response.js
@@ -2,7 +2,7 @@ module.exports = (status, data, callback) => {
   const response = {
     statusCode: status,
     headers: {
-      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Origin': 'https://download-your-travelmap.christianhaller.com',
     },
     body: JSON.stringify(data),
   };
--- infrastructure/gateway.tf
+++ infrastructure/gateway.tf
@@ -25,15 +25,24 @@ resource "aws_api_gateway_integration" "integration" {
 
 resource "aws_api_gateway_base_path_mapping" "test" {
   api_id      = "${aws_api_gateway_rest_api.api.id}"
-  stage_name  = "${aws_api_gateway_deployment.deployment.stage_name}"
+  stage_name  = "${aws_api_gateway_stage.stage.stage_name}"
   domain_name = "${aws_api_gateway_domain_name.api.domain_name}"
   depends_on = ["aws_api_gateway_method.method"]
 }
 
 // deployment
 
-resource "aws_api_gateway_deployment" "deployment" {
+resource "aws_api_gateway_stage" "stage" {
   depends_on = ["aws_api_gateway_integration.integration"]
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   stage_name = "prod"
+  cache_cluster_enabled = true
+  cache_cluster_size = 0.5
+  deployment_id = "${aws_api_gateway_deployment.deployment.id}"
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  stage_name = "prod"
+  depends_on = ["aws_api_gateway_integration.integration"]
 }
