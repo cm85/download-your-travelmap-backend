8399230b41dfeb907c7e27e383a31bc957878786
Christian Haller <christian.haller@otto.de>
2018-02-15 22:07:38 +0100

    been

--- app/csv.js
+++ app/csv.js
@@ -4,11 +4,15 @@ const json2csv = promisify(require('json2csv'));
 const fields = ['lat', 'lon', 'name', 'country', 'city', 'iso', 'been'];
 
 module.exports = (data) => {
-  const map = data.places.map((item) => {
-    item.been = item.flags.join(',');
-    item.lon = item.lng;
-    return item;
-  });
+  const map = data.places.map(item => ({
+    been: item.flags.join(','),
+    lat: item.lat,
+    name: item.name,
+    lon: item.lng,
+    country: item.country,
+    city: item.city,
+    iso: item.iso,
+  }));
 
   return json2csv({
     data: map,
