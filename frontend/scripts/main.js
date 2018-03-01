5a2bcd1c41f65c8f41a6fd955eec446fa8d03a0c
Christian Haller <christian.haller@otto.de>
2018-03-01 23:11:12 +0100

    sri

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,1 +1,3 @@
+require('../styles/main.scss');
+
 console.log('ee');
--- webpack.frontend.js
+++ webpack.frontend.js
@@ -1,12 +1,22 @@
 const path = require('path');
-const WebpackAssetsManifest = require('webpack-assets-manifest');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const SriPlugin = require('webpack-subresource-integrity');
+
+const extractSass = new ExtractTextPlugin({
+  filename: 'styles.[contenthash].css',
+});
+
 
 module.exports = {
-  entry: './frontend/scripts/main.js',
+  entry: {
+    js: './frontend/scripts/main.js',
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/dist',
+    crossOriginLoading: 'anonymous',
   },
   mode: 'production',
   module: {
@@ -22,11 +32,27 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [{
+            loader: 'css-loader',
+          }, {
+            loader: 'sass-loader',
+          }],
+        }),
+      },
     ],
   },
   plugins: [
-    new WebpackAssetsManifest({
-      merge: true,
+    new SriPlugin({
+      hashFuncNames: ['sha256', 'sha384'],
+      enabled: true,
     }),
+    new HtmlWebpackPlugin({
+      template: './frontend/template/index.html',
+      inject: false,
+    }),
+    extractSass,
   ],
 };
